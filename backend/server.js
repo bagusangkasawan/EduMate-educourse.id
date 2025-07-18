3b665bd83f51f35b889b0eae991f89da69efce21
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-07-19 01:57:55 +0700

    fix error vercel

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,9 @@ connectDB();
 
 const app = express();
 
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
