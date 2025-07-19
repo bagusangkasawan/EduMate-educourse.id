b8dd218fd86cbe2bb173ba7a496d9c45d2f79c23
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-07-19 20:29:17 +0700

    Fix: patch Rollup native module for Vercel build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+process.env.ROLLUP_FORCE_JS = 'true';
+
 export default defineConfig({
   plugins: [react()],
   server: {
