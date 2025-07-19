38cde578cf6d91fe10a7cbd6d7e2bc1a08307e66
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-07-19 20:43:30 +0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
+const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: 'https://edumate-educourse.vercel.app',
+  origin: allowedOrigin,
+  credentials: true
 }));
 
 app.get('/', (req, res) => {
