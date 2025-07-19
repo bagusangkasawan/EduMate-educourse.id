0b3dfc47d5f1c748dd3dd7efbc8287837f44b2d6
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-07-19 20:36:01 +0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: '*',
-}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://edumate-educourse.vercel.app',
+}));
 
 app.get('/', (req, res) => {
   res.send('EduMate API is running...');
